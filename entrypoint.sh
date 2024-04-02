649f7066d16566329741d2e23fcabcbde4b831db
Mark Stein <mystycs@gmail.com>
2024-04-01 20:34:57 -0400

    Sanity checks for dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,20 @@ fi
 # Start MariaDB
 /etc/init.d/mysql start
 
-#Set up secret.php file
-touch '/var/www/racktables/wwwroot/inc/secret.php'
+# Set up secret.php file if it doesn't exist
+SECRET_FILE='/var/www/racktables/wwwroot/inc/secret.php'
+if [ ! -f "$SECRET_FILE" ]; then
+    touch "$SECRET_FILE"
+    chmod a=rw "$SECRET_FILE"
+fi
 
-chmod a=rw '/var/www/racktables/wwwroot/inc/secret.php'
-
-# Install PHP extensions
-apt-get update && apt-get install -y \
-    php-gd php-mysql \
-    php-mbstring php-bcmath php-json php-snmp \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
+# Install PHP extensions if not already installed
+REQUIRED_EXTENSIONS="php-gd php-mysql php-mbstring php-bcmath php-json php-snmp"
+for EXTENSION in $REQUIRED_EXTENSIONS; do
+    if ! dpkg -l | grep -q "^ii  $EXTENSION"; then
+        apt-get install -y $EXTENSION
+    fi
+done
 
 # Start Apache2 in the foreground
 exec apache2ctl -D FOREGROUND
